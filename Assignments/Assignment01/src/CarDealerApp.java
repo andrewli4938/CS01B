7aedb67d77ad115a010ba8ef759cd8171ab5bb4e
Wei Li <w.li@salesforce.com>
2020-07-04 19:29:16 -0700

    Assignent 1 first commit

---
+++ Assignments/Assignment01/src/CarDealerApp.java
@@ -0,0 +1,12 @@
+
+public class CarDealerApp extends Object
+{
+
+    public static void main(String[] args) 
+    {
+        CarDealer dealer = new CarDealer();
+        dealer.init();
+        dealer.run();
+    }   
+
+}
